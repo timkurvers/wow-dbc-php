7bab0bbd8ec1c1e816c65332858b5846acc7527d
Tim Kurvers <tim@moonsphere.net>
2011-05-24 15:34:10 +0200

    Now licensed under the MIT license, updated accordingly

--- lib/DBCIterator.class.php
+++ lib/DBCIterator.class.php
@@ -8,19 +8,16 @@
  * required by the client to operate successfully and can be extracted
  * from the MPQ archives of the actual game client.
  * 
- * The contents of this file are subject to the Mozilla Public License
- * Version 1.1 (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
+ * The contents of this file are subject to the MIT License, under which 
+ * this library is licensed. See the LICENSE file for the full license.
  * 
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
- * License for the specific language governing rights and limitations
- * under the License.
- * 
- * Alternatively, the contents of this file may be used under the terms of
- * the GNU General Public License version 3 license (the "GPLv3"), in which
- * case the provisions of the GPLv3 are applicable instead of the above.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  * 
  * @author	Tim Kurvers <tim@moonsphere.net>
  */
